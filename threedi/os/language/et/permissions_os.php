31a1981a7bb7c97dac892eca0bdb630fe71a9969
phpBBEesti <argol76@gmail.com>
2019-08-06 08:27:17 +0300

    Update permissions_os.php

--- threedi/os/language/et/permissions_os.php
+++ threedi/os/language/et/permissions_os.php
@@ -6,7 +6,7 @@
  * @copyright (c) 2005 - 2019, 3Di, https://www.phpbbstudio.com
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
- * Estonian translation by phpBBestonia.eu <https://www.phpbbestonia.eu>
+ * Estonian translation by phpBBestonia.eu <https://www.phpbbestonia.eu> [Exabot @ 2019]
  */
 
 if (!defined('IN_PHPBB'))
